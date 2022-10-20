dd826ce02c5de3cf261495f43c4f9f47f5d56919
Nataliya Lazouskaya <93728977+NataliyaLaz@users.noreply.github.com>
2022-10-20 17:45:41 +0300

    Initial Commit

---
+++ Decorator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Decorator
+//
+//  Created by Nataliya Lazouskaya on 20.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
